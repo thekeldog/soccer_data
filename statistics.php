148b9d8c657a65e3d64b321753d1c73539db4cae
Kellen Rice <thekeldog@gmail.com>
2019-05-08 16:01:13 +0000

    more changes on stats page, forgot to branch

--- statistics.php
+++ statistics.php
@@ -13,6 +13,49 @@
 <div class="container" id="mainContainer">
 <body>
     <div class="container" id="listCont">
+        <div class = "row">
+        <div class="bd-example">
+          <div id="carouselExampleCaptions" class="carousel slide" data-ride="carousel">
+            <ol class="carousel-indicators">
+              <li data-target="#carouselExampleCaptions" data-slide-to="0" class="active"></li>
+              <li data-target="#carouselExampleCaptions" data-slide-to="1"></li>
+              <li data-target="#carouselExampleCaptions" data-slide-to="2"></li>
+            </ol>
+            <div class="carousel-inner">
+              <div class="carousel-item active">
+                <img src="media/f1.jpg" class="d-block w-500" alt="...">
+                <div class="carousel-caption d-none d-md-block">
+                  <h2>Statistics</h2>
+                  <p>Query Available Leagues and Teams and see past and present League Tables</p>
+                </div>
+              </div>
+              <div class="carousel-item">
+                <img src="media/f2.jpg" class="d-block w-500" alt="...">
+                <div class="carousel-caption d-none d-md-block">
+                  <h2>Statistics</h2>
+                  <p>Query Available Leagues and Teams and see past and present League Tables</p>
+                </div>
+              </div>
+              <div class="carousel-item">
+                <img src="media/f3.jpg" class="d-block w-500" alt="...">
+                <div class="carousel-caption d-none d-md-block">
+                  <h2>Statistics</h2>
+                  <p>Query Available Leagues and Teams and see past and present League Tables</p>
+                </div>
+              </div>
+            </div>
+            <a class="carousel-control-prev" href="#carouselExampleCaptions" role="button" data-slide="prev">
+              <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+              <span class="sr-only">Previous</span>
+            </a>
+            <a class="carousel-control-next" href="#carouselExampleCaptions" role="button" data-slide="next">
+              <span class="carousel-control-next-icon" aria-hidden="true"></span>
+              <span class="sr-only">Next</span>
+            </a>
+          </div>
+        </div>    
+        </div>
+        
         <div class="row">
         <h3>Select Data:</h3>
         <div class="dropdown">
