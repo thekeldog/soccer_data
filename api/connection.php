1fa80e53c3b5a62af3fcbf9304ef83c2741978b0
Kellen <thekeldog@gmail.com>
2019-05-10 14:40:08 -0700

    Heroku connect (#10)
    * messing with connection file
    
    * changed server setting to correct settings

--- api/connection.php
+++ api/connection.php
@@ -5,6 +5,8 @@ function get_database_connection($dbname = "soccer_dat") {
     $user_name = 'root';
     $password = "";
     
+
+    
     /*
     Host 	bfjrxdpxrza9qllq.cbetxkdyhwsb.us-east-1.rds.amazonaws.com 	
     Username 	sf1qzp17uzk5ms6f 	
@@ -16,17 +18,18 @@ function get_database_connection($dbname = "soccer_dat") {
     mysql://sf1qzp17uzk5ms6f:p6n7qso52n2og7b7@bfjrxdpxrza9qllq.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/lbkixx7usdyvo8hh
     */
     
+    
     if(strpos($_SERVER['HTTP_HOST'], 'herokuapp') !== false) {
        
-        $host = "bfjrxdpxrza9qllq.cbetxkdyhwsb.us-east-1.rds.amazonaws.com";
-        $dbname = "lbkixx7usdyvo8hh";
-        $username = "sf1qzp17uzk5ms6f";
-        $password = "p6n7qso52n2og7b7";
+        $host = "x3ztd854gaa7on6s.cbetxkdyhwsb.us-east-1.rds.amazonaws.com";
+        $dbname = "syx2c1kyb99wclfe";
+        $username = "udllv8c9u8gom2uq";
+        $password = "sg46k7eh9eammrdl";
     } 
 
     
     //create db connection
-    $dbConn = new PDO("mysql:host=$host;dbname=$dbname", $user_name, $password);
+    $dbConn = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
     $dbConn -> setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     
     return $dbConn;
