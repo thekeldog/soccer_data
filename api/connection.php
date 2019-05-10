fd027dd44772eeee94a0074bbf2a987546d60af6
Kellen Rice <thekeldog@gmail.com>
2019-05-10 21:05:49 +0000

    messing with connection file

--- api/connection.php
+++ api/connection.php
@@ -1,9 +1,14 @@
 <?php
 
 function get_database_connection($dbname = "soccer_dat") {
-    $host = "localhost"; //cloud 9
-    $user_name = 'root';
-    $password = "";
+    // $host = "localhost"; //cloud 9
+    // $user_name = 'root';
+    // $password = "";
+    
+    $host = "bfjrxdpxrza9qllq.cbetxkdyhwsb.us-east-1.rds.amazonaws.com";
+    $dbname = "lbkixx7usdyvo8hh";
+    $username = "sf1qzp17uzk5ms6f";
+    $password = "p6n7qso52n2og7b7";
     
     /*
     Host 	bfjrxdpxrza9qllq.cbetxkdyhwsb.us-east-1.rds.amazonaws.com 	
@@ -16,6 +21,8 @@ function get_database_connection($dbname = "soccer_dat") {
     mysql://sf1qzp17uzk5ms6f:p6n7qso52n2og7b7@bfjrxdpxrza9qllq.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/lbkixx7usdyvo8hh
     */
     
+    
+    
     if(strpos($_SERVER['HTTP_HOST'], 'herokuapp') !== false) {
        
         $host = "bfjrxdpxrza9qllq.cbetxkdyhwsb.us-east-1.rds.amazonaws.com";
@@ -26,7 +33,7 @@ function get_database_connection($dbname = "soccer_dat") {
 
     
     //create db connection
-    $dbConn = new PDO("mysql:host=$host;dbname=$dbname", $user_name, $password);
+    $dbConn = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
     $dbConn -> setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     
     return $dbConn;
